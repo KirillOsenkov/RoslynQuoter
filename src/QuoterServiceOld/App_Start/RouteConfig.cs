110fc02338fbdf99b329a8e151882e7557dbf3eb
Kirill Osenkov <github@osenkov.com>
2016-02-24 15:59:00 -0800

    Adding an old-style ASP.NET website in the interim until ASP.NET Core is ready.

---
+++ src/QuoterServiceOld/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace QuoterServiceOld
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute(""); // this is needed for the default document index.html to work
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Quoter", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/QuoterServiceOld/Controllers/QuoterController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace QuoterService.Controllers
+{
+    public class QuoterController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get(
+            string sourceText,
+            bool openCurlyOnNewLine = false,
+            bool closeCurlyOnNewLine = false,
+            bool preserveOriginalWhitespace = false,
+            bool keepRedundantApiCalls = false,
+            bool avoidUsingStatic = false)
+        {
+            string responseText = "Quoter is currently down for maintenance. Please check back later.";
+            if (string.IsNullOrEmpty(sourceText))
+            {
+                responseText = "Please specify the source text.";
+            }
+            else if (sourceText.Length > 2000)
+            {
+                responseText = "Only strings shorter than 2000 characters are supported; your input string is " + sourceText.Length + " characters long.";
+            }
+            else
+            {
+                try
+                {
+                    var quoter = new Quoter
+                    {
+                        OpenParenthesisOnNewLine = openCurlyOnNewLine,
+                        ClosingParenthesisOnNewLine = closeCurlyOnNewLine,
+                        UseDefaultFormatting = !preserveOriginalWhitespace,
+                        RemoveRedundantModifyingCalls = !keepRedundantApiCalls,
+                        ShortenCodeWithUsingStatic = !avoidUsingStatic
+                    };
+
+                    responseText = quoter.Quote(sourceText);
+                }
+                catch (Exception ex)
+                {
+                    responseText = ex.ToString();
+                }
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(responseText, Encoding.UTF8, "text/html");
+            return response;
+        }
+    }
+}
