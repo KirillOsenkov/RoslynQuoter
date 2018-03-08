c351d28644943c7ca4e4a6147a15abd74998dc1b
Kirill Osenkov <github@osenkov.com>
2018-03-08 11:59:45 -0800

    Port the website to AspNetCore 2.0.

---
+++ src/Quoter.Web/Controllers/QuoterController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuoterService.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuoterController : Controller
+    {
+        [HttpGet]
+        public string Get(
+            string sourceText,
+            NodeKind nodeKind = NodeKind.CompilationUnit,
+            bool openCurlyOnNewLine = false,
+            bool closeCurlyOnNewLine = false,
+            bool preserveOriginalWhitespace = false,
+            bool keepRedundantApiCalls = false,
+            bool avoidUsingStatic = false)
+        {
+            string prefix = null;
+
+            string responseText = "Quoter is currently down for maintenance. Please check back later.";
+            if (string.IsNullOrEmpty(sourceText))
+            {
+                responseText = "Please specify the source text.";
+            }
+            else if (sourceText.Length > 2000)
+            {
+                responseText = "Only strings shorter than 2000 characters are supported; your input string is " + sourceText.Length + " characters long.";
+            }
+            else
+            {
+                try
+                {
+                    var quoter = new Quoter
+                    {
+                        OpenParenthesisOnNewLine = openCurlyOnNewLine,
+                        ClosingParenthesisOnNewLine = closeCurlyOnNewLine,
+                        UseDefaultFormatting = !preserveOriginalWhitespace,
+                        RemoveRedundantModifyingCalls = !keepRedundantApiCalls,
+                        ShortenCodeWithUsingStatic = !avoidUsingStatic
+                    };
+
+                    responseText = quoter.Quote(sourceText, nodeKind);
+                }
+                catch (Exception ex)
+                {
+                    responseText = ex.ToString();
+
+                    prefix = "Congratulations! You've found a bug in Quoter! Please open an issue at <a href=\"https://github.com/KirillOsenkov/RoslynQuoter/issues/new\" target=\"_blank\">https://github.com/KirillOsenkov/RoslynQuoter/issues/new</a> and paste the code you've typed above and this stack:";
+                }
+            }
+
+            responseText = HttpUtility.HtmlEncode(responseText);
+
+            if (prefix != null)
+            {
+                responseText = "<div class=\"error\"><p>" + prefix + "</p><p>" + responseText + "</p><p><br/>P.S. Sorry!</p></div>";
+            }
+
+            return responseText;
+        }
+    }
+}
---
+++ src/Quoter.Web/wwwroot/scripts.js
@@ -0,0 +1,83 @@
+﻿function onPageLoad() {
+}
+
+function onSubmitClick() {
+    var nodeKind = document.getElementById("nodeKind").value;
+    var openCurlyOnNewLine = getCheckboxValue("openCurlyOnNewLine");
+    var closeCurlyOnNewLine = getCheckboxValue("closeCurlyOnNewLine");
+    var preserveOriginalWhitespace = getCheckboxValue("preserveOriginalWhitespace");
+    var keepRedundantApiCalls = getCheckboxValue("keepRedundantApiCalls");
+    var avoidUsingStatic = getCheckboxValue("avoidUsingStatic");
+    var query = "api/quoter/?sourceText=" + encodeURIComponent(inputBox.value);
+
+    query = query + "&nodeKind=" + nodeKind;
+
+    if (openCurlyOnNewLine) {
+        query = query + "&openCurlyOnNewLine=true";
+    }
+
+    if (closeCurlyOnNewLine) {
+        query = query + "&closeCurlyOnNewLine=true";
+    }
+
+    if (preserveOriginalWhitespace) {
+        query = query + "&preserveOriginalWhitespace=true";
+    }
+
+    if (keepRedundantApiCalls) {
+        query = query + "&keepRedundantApiCalls=true";
+    }
+
+    if (avoidUsingStatic) {
+        query = query + "&avoidUsingStatic=true";
+    }
+
+    getUrl(query, loadResults);
+}
+
+function getCheckboxValue(id) {
+    return document.getElementById(id).checked;
+}
+
+function getUrl(url, callback) {
+    enableSubmit(false);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Accept", "text/html");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            var data = xhr.responseText;
+            if (typeof data === "string" && data.length > 0) {
+                callback(data);
+            }
+        }
+
+        enableSubmit(true);
+    };
+    xhr.send();
+    return xhr;
+}
+
+function enableSubmit(enabled) {
+    var submitButton = document.getElementById("submitButton");
+    submitButton.disabled = !enabled;
+
+    var working = document.getElementById("working");
+    if (enabled) {
+        working.style.display = "none";
+    } else {
+        working.style.display = "inline";
+        setResult("");
+    }
+}
+
+function setResult(data) {
+    var container = document.getElementById("outputDiv");
+    if (container) {
+        container.innerHTML = data;
+    }
+}
+
+function loadResults(data) {
+    setResult(data);
+}
