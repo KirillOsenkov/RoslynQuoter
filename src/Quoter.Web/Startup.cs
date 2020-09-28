05f6fedc9b81491def72b578972b4a5442bd2556
Kirill Osenkov <github@osenkov.com>
2020-09-27 19:30:52 -0700

    Fix website warnings.

--- src/Quoter.Web/Startup.cs
+++ src/Quoter.Web/Startup.cs
@@ -23,17 +23,15 @@ namespace QuoterWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(o =>
+            {
+                o.EnableEndpointRouting = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseMvc();
             app.UseFileServer();
         }
